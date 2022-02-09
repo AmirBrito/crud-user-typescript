edf4c063c7650e1720e7e21a8d10e4c992aecbb7
amirbrito <amir.sbrito@gmail.com>
2022-02-08 22:28:54 -0300

    feature: entity user implemented, added dependencies uuid

---
+++ src/entities/User.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+export class User {
+    
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+
+    constructor() {
+        if(!this.id){
+            this.id = uuid();
+        }
+    }
+}
